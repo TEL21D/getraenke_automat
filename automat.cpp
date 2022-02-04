99aee127a043c722acc38af5975c8a43e1e2898b
I523036 <tobias.joschko@sap.com>
2022-02-04 11:08:32 +0100

    labor ende

--- automat.cpp
+++ automat.cpp
@@ -4,29 +4,91 @@
 
 // die verschiedenen Zustände des Getränkeautomaten
 enum struct zustand {
-
+    auswahl,
+    bezahlen,
+    ausgabe,
+    wartung,
+    ende
 };
 
 // die Datenstruktur eines Getränks
 struct s_getraenk
 {
-    /* _name */
-    /* _preis */
-    /* _anzahl */
+    std::string _name;
+    int _preis;
+    int _anzahl;
 };
 
+
+int getraenkAuswaehlen(const std::vector<s_getraenk> &getraenke);
+void getraenkBezahlen(const std::vector<s_getraenk> &getraenke);
+void getraenkAusgeben();
+void geldEinwerfen();
 
 int main(int argc, char const *argv[])
 {
     // Variable für den Zustand
+    zustand state = zustand::auswahl;
     // Ein Vector um die Getränke abzuspeichern
 
-    std::cout << "Bitte ein Getränk auswählen:\n";
+    std::vector<s_getraenk> getraenke = {
+        {"Cola", 250, 4},
+        {"Coke Zero", 200, 4},
+        {"Fanta", 250, 0},
+        {"Wasser", 100, 10}
+    };
 
-    while (true)
+    int auswahl = 0;
+
+    while (state != zustand::ende)
     {
         /* hier kommt der Zustandsautomat hin */
+        switch (state)
+        {
+        case zustand::auswahl:
+            auswahl = getraenkAuswaehlen(getraenke);
+            state = zustand::bezahlen;
+            break;
+        case zustand::bezahlen:
+            getreank
+            break;
+        case zustand::ausgabe:
+            /* code */
+            break;
+        case zustand::wartung:
+            /* code */
+            break;
+        case zustand::ende:
+            std::cout << "\nGetränkeautomat fährt herunter\n";
+            break;
+
+        default:
+            break;
+        }
     }
 
     return 0;
 }
+
+int getraenkAuswaehlen(const std::vector<s_getraenk> &getraenke) {
+    int in;
+    while (true)
+    {
+        std::cout << "\n Bitte wählen Sie ein Getränk aus:\n";
+        for (size_t i = 0; i < getraenke.size(); i++)
+        {
+            std::cout << " " << i+1 << ": " << getraenke[i]._name << " Preis: " << getraenke[i]._preis << "\n";
+        }
+        std::cin >> in;
+
+        if (getraenke[in-1]._anzahl >= 1)
+        {
+            return in-1;
+        }
+        else {
+            std::cout << "Das Getränk ist gerade nicht verfügbar. Treffen Sie bitte eine andere Auswahl!\n";
+        }
+    }
+}
+
+void getraenkBezahlen(const std::vector<s_getraenk> &getraenke);
